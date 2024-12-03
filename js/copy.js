9dc305221d04cab853fd42290cffdd3bec79ca43
Sequoia Haynes <supersequoia11@gmail.com>
2024-12-03 11:08:15 -0800

    oops

--- js/copy.js
+++ js/copy.js
@@ -2,11 +2,8 @@ var copyButton = document.getElementById("copyButton")
 var ipField = document.getElementById("ip")
 async function copyToClipboard(){
     //ik this function is deprecated, but the main clipboard api doesnt work for me
-    Document.execCommand("copy",false,ipField.value)
+    document.execCommand("copy",false,ipField.value)
     //im too lazy to go make a proper hovertext thing, il do it later
     alert("Copied!")
 }
-function silly(){
-    copyButton.addEventListener("click",copyToClipboard)
-}
-window.setTimeout(silly, 100);
+copyButton.addEventListener("click",copyToClipboard)
