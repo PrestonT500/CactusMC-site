c0e6d301665ca381afcc94a7aa1d308f58c97508
Sequoia Haynes <supersequoia11@gmail.com>
2024-12-02 18:47:23 -0800

    font fix

--- js/copy.js
+++ js/copy.js
@@ -1,7 +1,7 @@
 var copyButton = document.getElementById("copyButton")
 var ipField = document.getElementById("ip")
 async function copyToClipboard(){
-    await Clipboard.writeText(ipField.value)
+    await Navigator.clipboard.writeText(ipField.value)
     //im too lazy to go make a proper hovertext thing, il do it later
     alert("Copied!")
 }
