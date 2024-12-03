321d90aaf1da2be970ce278e3c0b848c05cb73cf
Sequoia Haynes <supersequoia11@gmail.com>
2024-12-03 10:33:40 -0800

    copy button should work this time

--- js/copy.js
+++ js/copy.js
@@ -1,7 +1,8 @@
 var copyButton = document.getElementById("copyButton")
 var ipField = document.getElementById("ip")
 async function copyToClipboard(){
-    await Navigator.clipboard.writeText(ipField.value)
+    //ik this function is deprecated, but the main clipboard api doesnt work for me
+    Document.execCommand("copy",false,ipField.value)
     //im too lazy to go make a proper hovertext thing, il do it later
     alert("Copied!")
 }
