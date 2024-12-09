be0c8db2bbaf9eeb2c9067c6ced893c7542d67b1
Sequoia Haynes <supersequoia11@gmail.com>
2024-12-08 19:16:51 -0800

    oops

--- js/copy.js
+++ js/copy.js
@@ -5,7 +5,7 @@ function sillyToClipboard(value) {
   }
 async function copyToClipboard(){
     //ik this function is deprecated, but the main clipboard api doesnt work for me
-    sillyToClipboard("ipField.value")
+    sillyToClipboard(ipField.value)
     //im too lazy to go make a proper hovertext thing, il do it later
     alert("Copied!")
 }
